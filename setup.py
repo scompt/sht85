75094258d77b13efb8a066b249fd683cb8541620
Edward Dale <scompt@scompt.com>
2020-07-06 17:18:51 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='SHT85',
+      version='1.0',
+      description='Python driver for Sensirion SHT85 sensors connected to I2c pins of a Raspberrry Pi',
+      author='jothanna',
+      author_email='jothanna@users.noreply.github.com',
+      url='https://github.com/jothanna/sht85',
+      packages=['sht85'],
+     )
